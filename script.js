5740d9906811b35eb912e8f086b852df2b8e42ad
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-20 15:57:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+// JavaScript code
+function fizzBuzz() {
+    var output = '';
+
+    for (var i = 1; i <= 20; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            output += 'FizzBuzz\n';
+        } else if (i % 3 === 0) {
+            output += 'Fizz\n';
+        } else if (i % 5 === 0) {
+            output += 'Buzz\n';
+        } else {
+            output += i + '\n';
+        }
+    }
+
+    return output;
+}
+
+// Display the result using alert
+document.getElementById('output').innerText = fizzBuzz();
+
